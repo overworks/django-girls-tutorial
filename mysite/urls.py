48e7a155598fad6e45057ddb56c12ffc71de5060
Minhyung Park <urlinee@gmail.com>
2020-01-04 23:34:39 +0900

    Added log in / log out features

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^accounts/login/$', views.login, name='login'),
+    path(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     path('', include('blog.urls')),
 ]
